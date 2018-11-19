22b27bed48e8204fed63b78159ac7bde1fb66cf7
Alessandro <alessandrotibaldi93@gmail.com>
2018-11-19 22:44:30 +0100

    Add Home, Contatti e Informazioni pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
+
+  def about
+  end
+
+  def contatti
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "Informazioni | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contatti" do
+    get static_pages_contatti_url
+    assert_response :success
+    assert_select "title", "Contatti | Ruby on Rails Tutorial Sample App"
   end
 
 end
