e9f9f78050dd11c7f049e029ce7d576f1c10c465
machine1104 <machine1104@gmail.com>
2018-11-20 11:33:17 +0000

    application helper creato

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @base_title = "Vieni, Vedi, Compra!"
+  end
+  
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "Informazioni | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Informazioni | #{@base_title}"
   end
 
   test "should get contatti" do
     get static_pages_contatti_url
     assert_response :success
-    assert_select "title", "Contatti | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contatti | #{@base_title}"
   end
 
 end
