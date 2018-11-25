4a7ed82ff743cb9d9e1cb8e389e17a59ac3e964c
machine1104 <machine1104@gmail.com>
2018-11-25 18:48:09 +0000

    Procedura di login completata

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Benvenuto "+@user.username
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root  'static_pages#home'
   get   '/about',             to: 'static_pages#about'
   get   '/contatti',          to: 'static_pages#contatti'
   get   '/registrazione',     to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   
 end
