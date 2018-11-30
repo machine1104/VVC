83edd42d11ab846e1ba13979ad0c49f8cfe2896c
machine1104 <machine1104@gmail.com>
2018-11-30 15:32:43 +0000

    cap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,9 +34,9 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :comune, :data_nascita)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :comune, :data_nascita)
+  end
     
      
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181121141322) do
+ActiveRecord::Schema.define(version: 20181130150001) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20181121141322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["username", "email"], name: "index_users_on_username_and_email", unique: true
   end
 
