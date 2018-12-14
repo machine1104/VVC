11bf5b1a4a8f4417db45f49ed184094e8c220352
Alessandro <alessandrotibaldi93@gmail.com>
2018-12-14 19:35:25 +0100

    Fix vari

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,24 +4,32 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
+    if !@user.nil?
+      if @user.persisted?
+        sign_in @user, :event => :authentication 
+        set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
+      end
+      redirect_to new_user_registration_url
     end
-    redirect_to '/'
+    flash[:danger] = "Esiste già un account con questa email"
+    redirect_to new_user_registration_url
   end
 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+    if !@user.nil?
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication 
+        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
+    flash[:danger] = "Esiste già un account con questa email"
+    redirect_to new_user_registration_url
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,14 @@ User.create!(username:  'admin',
     email: 'admin@vvc.com',
     password:              'password',
     password_confirmation: 'password',
-    comune: 'NY',
-    data_nascita: '1989-12-24',
     admin: true)
 
 100.times do |n|
     username = Faker::Internet.user_name(5..20)
     email = Faker::Internet.email
     password = Faker::Internet.password(8)
-    comune = Faker::Address.city
-    data_nascita = Faker::Date.birthday(18)
     User.create!(username:  username,
         email: email,
         password:              password,
-        password_confirmation: password,
-        comune: comune,
-        data_nascita: data_nascita)
+        password_confirmation: password)
 end
