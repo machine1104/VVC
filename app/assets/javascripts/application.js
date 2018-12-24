5df59f4dd6a932a34b86f306ab8120415e8a296c
Tibbo93 <alessandrotibaldi93@gmail.com>
2018-12-24 19:47:40 +0100

    Aggiornamento a Bootstrap 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require turbolinks
+//= require_tree
 //= require jquery3
-//= require jquery_ujs
-//= require_tree .
-//= require flatpickr
+//= require popper
 //= require bootstrap
