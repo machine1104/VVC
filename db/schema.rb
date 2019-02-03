04691041d69b887feb8fb0bff493f4106e1dd224
Tibbo93 <alessandrotibaldi93@gmail.com>
2019-02-03 16:24:24 +0100

    Fix vari

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20190202165321) do
     t.float "prezzo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "immagine"
     t.string "immagine_1"
     t.string "immagine_2"
     t.string "immagine_3"
@@ -92,5 +91,4 @@ ActiveRecord::Schema.define(version: 20190202165321) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
